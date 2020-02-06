c7256146a1986ad1620ace407f3a17323e9cb646
￠幻冰 <huanbing@wacai.com>
2020-02-06 22:19:37 +0800

    下拉菜单：拍照+相册

--- SwiftUI-Guide/Advance/Photo/PhotoAlbumView.swift
+++ SwiftUI-Guide/Advance/Photo/PhotoAlbumView.swift
@@ -9,22 +9,33 @@
 import SwiftUI
 
 struct PhotoAlbumView: View {
+  @State var showActionSheet = false
   @State var showImagePicker = false
   @State var image: Image? = nil
   @State var sourceType: UIImagePickerController.SourceType?
   
   var body: some View {
     VStack {
-      Button("拍照") {
-        self.sourceType = .camera
-        self.showImagePicker = true
-      }
-      .padding()
-      Button("从手机相册选择") {
-        self.sourceType = .photoLibrary
-        self.showImagePicker = true
+      Button("换头像") {
+        self.showActionSheet = true
       }
       image?.resizable().scaledToFit().padding()
+    }
+    .actionSheet(isPresented: $showActionSheet) {
+      .init(
+        title: .init("选择一张图片"),
+        buttons: [
+          .default(Text("拍 照")) {
+            self.sourceType = .camera
+            self.showImagePicker = true
+          },
+          .default(Text("从手机相册选择")) {
+            self.sourceType = .photoLibrary
+            self.showImagePicker = true
+          },
+          .cancel(Text("取 消"))
+        ]
+      )
     }
     .sheet(isPresented: $showImagePicker) {
       ImagePicker(image: self.$image, sourceType: self.sourceType)
