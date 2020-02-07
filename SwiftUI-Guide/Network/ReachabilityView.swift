c7df46764a306898d9fcb759daa5700751fe37a8
￠幻冰 <huanbing@wacai.com>
2020-02-07 19:37:19 +0800

    检测网络的连接状态-本地化

--- SwiftUI-Guide/Network/ReachabilityView.swift
+++ SwiftUI-Guide/Network/ReachabilityView.swift
@@ -13,15 +13,18 @@ struct ReachabilityView: View {
   private let reachability = try! Reachability()
   @State var connectionStatus: String?
   
+  func closure(reachability: Reachability) {
+    self.connectionStatus = NSLocalizedString(
+      reachability.connection.description,
+      comment: "网络状态"
+    )
+  }
+  
   var body: some View {
     Text("网络状态：\(connectionStatus ?? "获取中")")
       .onAppear() {
-        self.reachability.whenReachable = {
-          self.connectionStatus = $0.connection.description
-        }
-        self.reachability.whenUnreachable = {
-          self.connectionStatus = $0.connection.description
-        }
+        self.reachability.whenReachable = self.closure
+        self.reachability.whenUnreachable = self.closure
         do {
           try self.reachability.startNotifier()
         } catch {
