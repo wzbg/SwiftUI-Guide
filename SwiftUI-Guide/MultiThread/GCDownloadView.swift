97adb140f57d911b6af6b67c2cbee7b51f26442e
￠幻冰 <huanbing@wacai.com>
2020-02-08 20:42:16 +0800

    GCD下载

---
+++ SwiftUI-Guide/MultiThread/GCDownloadView.swift
@@ -0,0 +1,39 @@
+//
+//  GCDownloadView.swift
+//  SwiftUI-Guide
+//
+//  Created by huanbing on 2020/2/8.
+//  Copyright © 2020 unrealce. All rights reserved.
+//
+
+import SwiftUI
+
+struct GCDownloadView: View {
+  let placeholderOne = UIImage(named: "avarta3")
+  @State private var remoteImage : UIImage? = nil
+  @State private var errorMessage : String = ""
+  
+  var body: some View {
+    VStack {
+      Image(uiImage: self.remoteImage ?? placeholderOne!)
+        .onAppear() {
+          guard let url = URL(string: "http://hdjc8.com/images/logo.png") else {
+            return
+          }
+          DispatchQueue.global().async {
+            guard let data = try? Data(contentsOf: url) else {
+              return
+            }
+            self.remoteImage = UIImage(data: data)
+          }
+        }
+      Text(errorMessage).padding()
+    }
+  }
+}
+
+struct GCDownloadView_Previews: PreviewProvider {
+  static var previews: some View {
+    GCDownloadView()
+  }
+}
