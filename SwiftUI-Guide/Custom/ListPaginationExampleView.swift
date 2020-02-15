5984a5923daae967f2dbe69bad30cb7beb01a462
￠幻冰 <huanbing@wacai.com>
2020-02-15 14:33:23 +0800

    SwiftUI 高级List分页与无限滚动之基础版

---
+++ SwiftUI-Guide/Custom/ListPaginationExampleView.swift
@@ -0,0 +1,91 @@
+//
+//  ListPaginationExampleView.swift
+//  SwiftUI-Guide
+//
+//  Created by huanbing on 2020/2/15.
+//  Copyright © 2020 unrealce. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color: Identifiable {
+  public var id: Color {
+    self
+  }
+}
+
+extension RandomAccessCollection where Element: Identifiable {
+  func isFirst<Item: Identifiable>(_ item: Item) -> Bool {
+    guard !isEmpty else {
+      return false
+    }
+    guard let currentIndex = firstIndex(where: {
+      $0.id.hashValue == item.id.hashValue
+    }) else {
+      return false
+    }
+    let distance = self.distance(from: startIndex, to: currentIndex)
+    return distance == 0
+  }
+  
+  func isLast<Item: Identifiable>(_ item: Item) -> Bool {
+    guard !isEmpty else {
+      return false
+    }
+    guard let currentIndex = lastIndex(where: {
+      $0.id.hashValue == item.id.hashValue
+    }) else {
+      return false
+    }
+    let distance = self.distance(from: currentIndex, to: endIndex)
+    return distance == 1
+  }
+}
+
+struct ListPaginationExampleView: View {
+  @State private var items = [randomColor()]
+  @State private var isLoading = false
+  
+  var body: some View {
+    VStack {
+//      if isLoading {
+//        Text("Loading")
+//      }
+      List(items) { item in
+        Rectangle()
+          .foregroundColor(item)
+          .onAppear() {
+            self.listItemAppears(item)
+          }
+      }
+      if isLoading {
+        Text("Loading")
+      }
+    }
+  }
+  
+  private func listItemAppears<Item: Identifiable>(_ item: Item) {
+    guard items.isLast(item) else {
+      return
+    }
+    isLoading = true
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+      self.items.append(Self.randomColor())
+      self.isLoading = false
+    }
+  }
+  
+  private static func randomColor() -> Color {
+    Color(red: randomDouble(), green: randomDouble(), blue: randomDouble())
+  }
+  
+  private static func randomDouble() -> Double {
+    Double.random(in: 0 ... 1)
+  }
+}
+
+struct ListPaginationExampleView_Previews: PreviewProvider {
+  static var previews: some View {
+    ListPaginationExampleView()
+  }
+}
