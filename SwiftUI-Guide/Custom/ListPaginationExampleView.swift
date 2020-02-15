8e759fd58accb66a7a1993778056dcae84dd5fa1
￠幻冰 <huanbing@wacai.com>
2020-02-15 15:19:37 +0800

    SwiftUI 高级List分页与无限滚动之高级版

--- SwiftUI-Guide/Custom/ListPaginationExampleView.swift
+++ SwiftUI-Guide/Custom/ListPaginationExampleView.swift
@@ -5,6 +5,8 @@
 //  Created by huanbing on 2020/2/15.
 //  Copyright © 2020 unrealce. All rights reserved.
 //
+//  https://www.jianshu.com/p/f1e7411a388a
+//  https://www.jianshu.com/p/7005969b5f2e
 
 import SwiftUI
 
@@ -20,7 +22,7 @@ extension RandomAccessCollection where Element: Identifiable {
       return false
     }
     guard let currentIndex = firstIndex(where: {
-      $0.id.hashValue == item.id.hashValue
+      AnyHashable($0.id) == AnyHashable(item.id)
     }) else {
       return false
     }
@@ -33,18 +35,34 @@ extension RandomAccessCollection where Element: Identifiable {
       return false
     }
     guard let currentIndex = lastIndex(where: {
-      $0.id.hashValue == item.id.hashValue
+      AnyHashable($0.id) == AnyHashable(item.id)
     }) else {
       return false
     }
     let distance = self.distance(from: currentIndex, to: endIndex)
     return distance == 1
   }
+  
+  func isThresholdItem<Item: Identifiable>(offset: Int, item: Item) -> Bool {
+    guard !isEmpty else {
+      return false
+    }
+    guard let currentIndex = lastIndex(where: {
+      AnyHashable($0.id) == AnyHashable(item.id)
+    }) else {
+      return false
+    }
+    let distance = self.distance(from: currentIndex, to: endIndex)
+    let offset = offset < count ? offset : count - 1
+    print("currentIndex: \(currentIndex), endIndex: \(endIndex), distance: \(distance), offset: \(offset)")
+    return distance > offset
+  }
 }
 
 struct ListPaginationExampleView: View {
   @State private var items = [randomColor()]
   @State private var isLoading = false
+  private let offset = 10
   
   var body: some View {
     VStack {
@@ -65,9 +83,12 @@ struct ListPaginationExampleView: View {
   }
   
   private func listItemAppears<Item: Identifiable>(_ item: Item) {
-    guard items.isLast(item) else {
+    guard items.isThresholdItem(offset: offset, item: item) else {
       return
     }
+//    guard items.isLast(item) else {
+//      return
+//    }
     isLoading = true
     DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
       self.items.append(Self.randomColor())
